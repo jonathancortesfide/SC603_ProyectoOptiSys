b23deeb21796b8693af40caf6996bb2c76951c80
Jonathan Cortes <jcortes20382@ufide.ac.cr>
2026-04-21 18:05:46 -0600

    Updated layout

--- SPA-Elixir/Desarrollo/src/layouts/full/vertical/sidebar/MenuItems.js
+++ SPA-Elixir/Desarrollo/src/layouts/full/vertical/sidebar/MenuItems.js
@@ -1,12 +1,9 @@
 import { uniqueId } from 'lodash';
 
 import {
-  IconBoxMultiple,
   IconAperture,
   IconSettings,
   IconCurrencyDollar,
-  IconTag,
-  IconUserCheck,
   IconUsers,
   IconPackage
 } from '@tabler/icons';
@@ -46,33 +43,9 @@ const Menuitems = [
       },
       {
         id: uniqueId(),
-        title: 'Marcas',
-        icon: IconTag,
-        href: '/mantenimientos/marca',
-      },
-      {
-        id: uniqueId(),
-        title: 'Listas de precio',
-        icon: IconCurrencyDollar,
-        href: '/mantenimientos/lista-precio',
-      },
-      {
-        id: uniqueId(),
         title: 'Tipo de lente',
         icon: IconAperture,
         href: '/mantenimientos/tipo-lente',
-      },
-      {
-        id: uniqueId(),
-        title: 'Clasificación pacientes',
-        icon: IconUserCheck,
-        href: '/mantenimientos/clasificacion-pacientes',
-      },
-      {
-        id: uniqueId(),
-        title: 'Grupos de productos',
-        icon: IconBoxMultiple,
-        href: '/mantenimientos/grupos-productos',
       },
     ],
   },
